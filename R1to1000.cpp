8604f870d3ec81b07d451024728ed5053cde4d3f
Thomas Santos <thomas@socialeffort.co>
2017-10-16 14:35:49 -0400

    start func randGame

--- R1to1000.cpp
+++ R1to1000.cpp
@@ -7,3 +7,18 @@ using namespace std;
 
 void randGame(); // prototype
 bool isRight( int, int ); // prototype
+
+// func randGame selects int b/w 1 and 1000
+// and checks the user's answer
+
+void randGame()
+{
+	int answer; // the random int
+	int guess; // the user input
+	char response; // prompty 'y' or 'n' to continue
+
+	// user loop that runs until user quits w/ 'n'
+	do {
+
+	}
+}
