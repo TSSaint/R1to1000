ee3c99a8cab0e36ea7f10992c042b8f73a2e3014
Thomas Santos <thomas@socialeffort.co>
2017-10-16 14:31:02 -0400

    add prototypes

---
+++ R1to1000.cpp
@@ -0,0 +1,9 @@
+// User prompted to guess for a number b/w 1 and 1000
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void randGame(); // prototype
+bool isRight( int, int ); // prototype
